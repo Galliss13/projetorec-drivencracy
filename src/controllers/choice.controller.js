936526cb1c6d911bd6b646a783e06a69c762dad8
Galliss13 <matheusgalindo1313@gmail.com>
2022-11-30 15:40:53 -0300

    feat: postChoice e getPollChoices criados e funcionando

--- src/controllers/choice.controller.js
+++ src/controllers/choice.controller.js
@@ -1,10 +1,28 @@
-import { choiceCollection } from "../database/db"
+import { choiceCollection } from "../database/db.js"
 
 export async function postChoice(req, res) {
-
+    const choice = res.locals.choice
+    try {
+        await choiceCollection.insertOne(choice)
+        res.status(201).send(choice.title)
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(500)
+    }
 }
 
-export async function getPollChoices() {
+export async function getPollChoices(req, res) {
+    const {id} = req.params
+    try {
+        const pollChoices = await choiceCollection.find({pollId: id}).toArray()
+        if (pollChoices) {
+            return res.status(200).send(pollChoices)
+        }
+        return res.sendStatus(404)
+    } catch(err) {
+        console.log(err);
+        return res.sendStatus(500)
+    }
 }
 
 export async function postChoiceVote() {
